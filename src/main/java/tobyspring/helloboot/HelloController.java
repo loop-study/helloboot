ce6bebd2c69cfa6679a0a4a053c2111a4f36493c
apdkeb <apdkeb@naver.com>
2023-02-22 21:56:49 +0900

    디스패쳐 서블릿과 스프링 컨테이너 사용

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,13 +1,20 @@
 package tobyspring.helloboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RestController
 public class HelloController {
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
 
     @GetMapping("/hello")
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello(String name) {
-        return "hello " + name;
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,16 +1,14 @@
 package tobyspring.helloboot;
 
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -21,41 +19,65 @@ import java.io.IOException;
 public class HellobootApplication {
 
     public static void main(String[] args) {
-        GenericApplicationContext applicationContext = new GenericApplicationContext();
-        applicationContext.registerBean(HelloController.class); // bean 등록
-        applicationContext.refresh(); // bean 생성
+        GenericWebApplicationContext applicationContext = new GenericWebApplicationContext() {
+            @Override
+            protected void onRefresh() {
+                // 생략하면 안됨. GenericWebApplicationContext에서 추가적인 작업을 하기 때문.
+                super.onRefresh();
 
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                WebServer webServer = serverFactory.getWebServer(servletContext -> {
+                    servletContext.addServlet("dispatcherServlet",
+                            new DispatcherServlet(this)
+                    ).addMapping("/*");
+                });
 
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // 프론트 컨트롤러에서는
-                    // 인증, 보안, 다국어, 공통 기능을 처리함.
+                webServer.start();
+            }
+        };
 
-                    // 중요한 작업 2가지
-                    // 1. 매핑 - 어떤 로직을 호출하고 리턴된 값을 보내줄 것인가?
-                    // 2. 바인딩 - 어떻게 요청에 담긴 데이터의 유형을 파악하고 넣어줄 것인가?
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
-                        String name = req.getParameter("name");
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.registerBean(SimpleHelloService.class);
+        applicationContext.refresh();
 
-                        HelloController helloController = applicationContext.getBean(HelloController.class);
-                        String ret = helloController.hello(name);
+        //
+//        GenericApplicationContext applicationContext = new GenericApplicationContext();
+//
+//        applicationContext.registerBean(HelloController.class);
+//        applicationContext.registerBean(SimpleHelloService.class);
+//        applicationContext.refresh();
 
-                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println(ret);
-                    } else {
-                        // 해당 되는게 없다면 404를 뿌려주자.
-                        resp.setStatus(HttpStatus.NOT_FOUND.value());
-                    }
+//        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+//        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+//
+//            servletContext.addServlet("frontcontroller", new HttpServlet() {
+//                @Override
+//                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//                    // 프론트 컨트롤러에서는
+//                    // 인증, 보안, 다국어, 공통 기능을 처리함.
+//
+//                    // 중요한 작업 2가지
+//                    // 1. 매핑 - 어떤 로직을 호출하고 리턴된 값을 보내줄 것인가?
+//                    // 2. 바인딩 - 어떻게 요청에 담긴 데이터의 유형을 파악하고 넣어줄 것인가?
+//                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+//                        String name = req.getParameter("name");
+//
+//                        HelloController helloController = applicationContext.getBean(HelloController.class);
+//                        String ret = helloController.hello(name);
+//
+//                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+//                        resp.getWriter().println(ret);
+//                    } else {
+//                        // 해당 되는게 없다면 404를 뿌려주자.
+//                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+//                    }
+//
+//                }
+//            }).addMapping("/*"); // /* 로 지정하면 모든 경로가 들어옴.
+////            }).addMapping("/hello");
+//        });
 
-                }
-            }).addMapping("/*"); // /* 로 지정하면 모든 경로가 들어옴.
-//            }).addMapping("/hello");
-        });
-
-        webServer.start();
+//        webServer.start();
     }
 
 }
