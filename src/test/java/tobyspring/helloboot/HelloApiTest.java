74bc89c389df2e59bf372118c2fa2c7efb2d2bba
apdkeb <apdkeb@naver.com>
2023-02-26 23:20:46 +0900

    테스트 코드를 이용한 테스트

---
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -0,0 +1,36 @@
+package tobyspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloApiTest {
+
+    @Test
+    void helloApi() {
+        // http localhost:8080/hello?name=Spring
+        // HTTPie
+        TestRestTemplate rest = new TestRestTemplate();
+
+        // 웹의 모든 응답 요소를 담고 있음.
+        ResponseEntity<String> res =
+                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        // status code 200
+        assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        // header(content-type) text/plain
+        assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+
+        // body hello Spring
+        assertThat(res.getBody()).isEqualTo("hello Spring");
+    }
+}
