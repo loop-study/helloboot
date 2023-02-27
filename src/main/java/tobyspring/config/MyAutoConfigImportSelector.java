adfae6de2804ce246e465bb034f261abc58d8336
apdkeb <apdkeb@naver.com>
2023-02-28 00:23:36 +0900

    자동 구성 정보 파일 분리.

--- src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
+++ src/main/java/tobyspring/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,22 @@
 package tobyspring.config;
 
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
-public class MyAutoConfigImportSelector implements DeferredImportSelector {
+import java.util.stream.StreamSupport;
+
+public class MyAutoConfigImportSelector implements DeferredImportSelector { // BeanClassLoaderAware를 impl 받아서 해도 됨.
+    private final ClassLoader classLoader;  // 생성자 주입 방법.
+
+    public MyAutoConfigImportSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "tobyspring.config.autoconfig.DispatcherServletConfig",
-                "tobyspring.config.autoconfig.TomcatWebServerConfig"
-        };
+        Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);// 스프링 컨테이너가 빈을 로딩하는 클래스로더를 넣어줌.
+        return StreamSupport.stream(candidates.spliterator(), false).toArray(String[]::new);
     }
 }
